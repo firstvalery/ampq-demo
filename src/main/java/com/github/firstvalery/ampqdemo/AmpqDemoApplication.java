806ffc211c05d7d64a872192e372887ead56f36c
firstvalery <firstvalery@gmail.com>
2022-08-04 10:59:49 +0300

    Add existing project files to Git

---
+++ src/main/java/com/github/firstvalery/ampqdemo/AmpqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.firstvalery.ampqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpqDemoApplication.class, args);
+	}
+
+}
